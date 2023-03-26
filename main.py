27252e0c542c1541053ccbe30e8d6ef1fd5bd07e
SiDK <127277028+SiDK-FM@users.noreply.github.com>
2023-03-26 12:16:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -37,12 +37,12 @@ def loopthread(message):
     if len(urls) == 0: return
 
     if bypasser.ispresent(ddllist,urls[0]):
-        msg = app.send_message(message.chat.id, "⚡ __generating...__", reply_to_message_id=message.id)
+        msg = app.send_message(message.chat.id, "⚡ __generating...\n\nᴊᴏɪɴ ғᴏʀ ᴍᴏʀᴇ ʙᴏᴛs :- @MensBotZ__", reply_to_message_id=message.id)
     else:
         if urls[0] in "https://olamovies" or urls[0] in "https://psa.pm/":
-            msg = app.send_message(message.chat.id, "🔎 __this might take some time...__", reply_to_message_id=message.id)
+            msg = app.send_message(message.chat.id, "🔎 __ᴛʜɪs ᴍɪɢʜᴛ ᴛᴀᴋᴇ sᴏᴍᴇ ᴛɪᴍᴇ...__", reply_to_message_id=message.id)
         else:
-            msg = app.send_message(message.chat.id, "🔎 __bypassing...__", reply_to_message_id=message.id)
+            msg = app.send_message(message.chat.id, "🔎 __ʙʏᴘᴀssɪɴɢ...__", reply_to_message_id=message.id)
 
     link = ""
     for ele in urls:
@@ -55,10 +55,10 @@ def loopthread(message):
         else:    
             try: temp = bypasser.shortners(ele)
             except Exception as e: temp = "**Error**: " + str(e)
-        print("bypassed:",temp)
+        print("ʙʏᴘᴀssᴇᴅ:",temp)
         link = link + temp + "\n\n"
         
-    try: app.edit_message_text(message.chat.id, msg.id, f'__{link}__', disable_web_page_preview=True)
+    try: app.edit_message_text(message.chat.id, msg.id, f"__ʜᴇʀᴇ ɪs ʏᴏᴜʀ ᴅᴇsᴛɪɴᴀᴛɪᴏɴ ʟɪɴᴋ \n\n__{link}__\n\n©️ᴅᴇᴠ :- @MensBotZ", disable_web_page_preview=True)
     except:
         try: app.edit_message_text(message.chat.id, msg.id, "__Failed to Bypass__")
         except:
@@ -70,12 +70,12 @@ def loopthread(message):
 # start command
 @app.on_message(filters.command(["start"]))
 def send_start(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
-    app.send_message(message.chat.id, f"__👋 Hi **{message.from_user.mention}**, i am Link Bypasser Bot, just send me any supported links and i will you get you results.\nCheckout /help to Read More__",
-    reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton("🌐 Source Code", url="https://github.com/bipinkrish/Link-Bypasser-Bot")]]), reply_to_message_id=message.id)
+    app.send_message(message.chat.id, f"__👋 ʜɪ **{message.from_user.mention}**, ɪ ᴀᴍ ʟɪɴᴋ ʙʏᴘᴀssᴇʀ ʙᴏᴛ, ᴊᴜsᴛ sᴇɴᴅ ᴍᴇ ᴀɴʏ sᴜᴘᴘᴏʀᴛᴇᴅ ʟɪɴᴋs ᴀɴᴅ ɪ ᴡɪʟʟ ʏᴏᴜ ɢᴇᴛ ʏᴏᴜ ᴏʀɪɢɪɴᴀʟ ʟɪɴᴋ.\nᴄʜᴇᴄᴋᴏᴜᴛ /help ᴛᴏ ʀᴇᴀᴅ ᴍᴏʀᴇ\n\nᴀɴʏ ɪssᴜᴇ sᴏ ᴄᴏɴᴛᴀᴄᴛ ʜᴇʀᴇ\n©️ᴅᴇᴠ :- @Fm_Onr 🕊",
+    reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton("📯υρ∂αтє ¢нαηηєℓ 🦋 ", url="https://t.me/MensBotZ")]]), reply_to_message_id=message.id)
 
 
 # help command
-@app.on_message(filters.command(["help"]))
+@app.on_message(filters.command(["help"])
 def send_help(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
     app.send_message(message.chat.id, HELP_TEXT, reply_to_message_id=message.id, disable_web_page_preview=True)
 
@@ -90,13 +90,13 @@ def receive(client: pyrogram.client.Client, message: pyrogram.types.messages_and
 # doc thread
 def docthread(message):
     if message.document.file_name.endswith("dlc"):
-        msg = app.send_message(message.chat.id, "🔎 __bypassing...__", reply_to_message_id=message.id)
+        msg = app.send_message(message.chat.id, "🔍 __bypassing\n\nᴊᴏɪɴ ғᴏʀ ᴍᴏʀᴇ ᴀᴍᴀᴢɪɴɢ ʙᴏᴛs :- @MensBotZ...__", reply_to_message_id=message.id)
         print("sent DLC file")
         sess = requests.session()
         file = app.download_media(message)
         dlccont = open(file,"r").read()
         link = bypasser.getlinks(dlccont,sess)
-        app.edit_message_text(message.chat.id, msg.id, f'__{link}__')
+        app.edit_message_text(message.chat.id, msg.id, f' ʜᴇʀᴇ ɪs ʏᴏᴜʀ ᴅᴇsᴛɪɴᴀᴛɪᴏɴ ʟɪɴᴋ 🔗\n\n__{link}__\n\©️ᴅᴇᴠ :- @Fm_Onr')
         os.remove(file)
 
 
